d2a997833e9758aecf0b9df51048e8063294fb20
Pierre Maoui <maouipierre+github@gmail.com>
2016-11-11 14:56:23 +0100

    rm symlinks before

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 
-sudo apt-get install neovim tmux
+sudo apt-get install -y neovim tmux
 
+rm ~/.zshrc ~/.vimrc ~/.tmux.conf
 ln -s $ABSDIR/zshrc ~/.zshrc
 ln -s $ABSDIR/vimrc ~/.vimrc
 ln -s $ABSDIR/tmux.conf ~/.tmux.conf
