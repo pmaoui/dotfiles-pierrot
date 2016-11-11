8b54bbff0fcd390f2d5082b8536539d07a883d9f
Pierre Maoui <maouipierre+github@gmail.com>
2016-11-11 15:03:35 +0100

    remove nvim links

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ ln -s $ABSDIR/zshrc ~/.zshrc
 ln -s $ABSDIR/vimrc ~/.vimrc
 ln -s $ABSDIR/tmux.conf ~/.tmux.conf
 
-mkdir ~/.vim
+mkdir ~/.vim 2>/dev/null
+rm ~/.config/nvim
 ln -s ~/.config/nvim ~/.vim
 ln -s ~/.vim/init.vim ~/.vimrc
