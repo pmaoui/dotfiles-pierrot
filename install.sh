b2c40ba5d57e61248f0a9407cfe993968ede8a63
Pierre Maoui <maouipierre+github@gmail.com>
2016-11-11 15:43:42 +0100

    fix tabs

--- install.sh
+++ install.sh
@@ -10,12 +10,16 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update
 sudo apt-get install -y neovim tmux zsh
 
+
 rm ~/.zshrc ~/.vimrc ~/.tmux.conf
 ln -s $ABSDIR/zshrc ~/.zshrc
 ln -s $ABSDIR/vimrc ~/.vimrc
 ln -s $ABSDIR/tmux.conf ~/.tmux.conf
 
 mkdir ~/.vim 2>/dev/null
+if [ ! -d ~/.config ]; then
+  mkdir ~/.config
+fi
 rm -R ~/.config/nvim 2>/dev/null
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.vim/init.vim
