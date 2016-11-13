5671fe7f0c60f8778634a6b904c66e0443a56a94
Pierre Maoui <maouipierre+github@gmail.com>
2016-11-13 18:48:45 +0100

    add rxvt

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@ git submodule update --init --recursive
 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update
-sudo apt-get install -y neovim tmux zsh
+sudo apt-get install -y neovim tmux zsh xfonts-terminus fonts-inconsolata rxvt-unicode-256colors
 
 
-rm ~/.zshrc ~/.vimrc ~/.tmux.conf
+rm ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.Xdefaults
 ln -s $ABSDIR/zshrc ~/.zshrc
 ln -s $ABSDIR/vimrc ~/.vimrc
 ln -s $ABSDIR/tmux.conf ~/.tmux.conf
+ln -s $ABSDIR/Xdefaults ~/.Xdefaults
 
 mkdir ~/.vim 2>/dev/null
 if [ ! -d ~/.config ]; then
