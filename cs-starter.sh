0d03bd00470e7398d977fa0ab1d0a594d40042de
Pierre Maoui <pierre.maoui@contentsquare.com>
2017-07-17 11:04:55 +0200

    undo nvim term only

--- cs-starter.sh
+++ cs-starter.sh
@@ -9,40 +9,38 @@ cd ~/Public/mousetest/
 ##### TMUX SESSION #####
 ########################
 
-## -d : any other clients are detached
-#tmux new-session -s "$sn" -n dev-env -d
+# -d : any other clients are detached
+tmux new-session -s "$sn" -n dev-env -d
 
-## split the window (-)
-#tmux split-window -v -t "$sn"
+# split the window (-)
+tmux split-window -v -t "$sn"
 
-## split the first pane and the second (|)
-## tips : q to see the pane number
-#tmux split-window -h -t "$sn:.0"
-#tmux split-window -h -t "$sn:.2"
+# split the first pane and the second (|)
+# tips : q to see the pane number
+tmux split-window -h -t "$sn:.0"
+tmux split-window -h -t "$sn:.2"
 
-## upper-left
-#tmux send-keys -t "$sn:.0" "../cs-env/dev/containers.sh start" Enter
-## lower-left
-#tmux send-keys -t "$sn:.1" "./start-webdriver.sh" Enter
-## upper-right
-#tmux send-keys -t "$sn:.2" "grunt dev" Enter
+# upper-left
+tmux send-keys -t "$sn:.0" "../cs-env/dev/containers.sh start" Enter
+# lower-left
+tmux send-keys -t "$sn:.1" "./start-webdriver.sh" Enter
+# upper-right
+tmux send-keys -t "$sn:.2" "grunt dev" Enter
 
-#tmux new-window -t "$sn" -n dev
-#tmux select-window -t "$sn:0"
+tmux new-window -t "$sn" -n dev
+tmux select-window -t "$sn:0"
 
-#tmux attach -t "$sn"
-
+tmux attach -t "$sn"
 
 ########################
 ##### NVIM SESSION #####
 ########################
-
-nvim \
-  -c "split" \
-  -c "vsplit" \
-  -c "terminal ../cs-env/dev/containers.sh start" \
-  -c "wincmd l" \
-  -c "terminal ./start-webdriver.sh" \
-  -c "wincmd j" \
-  -c "terminal grunt dev" \
-  -c "tabnew" \
+# 1000: jump at the bottom, enable autoscroll (:1000)
+# nvim \
+#  -c "split | vsplit" \
+#  -c "terminal ../cs-env/dev/containers.sh start" -c "100" \
+#  -c "wincmd l" \
+#  -c "terminal ./start-webdriver.sh" -c "100" \
+#  -c "wincmd j" \
+#  -c "terminal grunt dev" -c "100" \
+#  -c "tabnew | stopinsert"
