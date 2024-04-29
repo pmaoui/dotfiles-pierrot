c5d1b75708ef7919097c6f188b20b5ef65dfd2aa
Pierrot <pierre@qwarry.com>
2024-04-29 23:37:58 +0200

    back to basic

--- nvim-conf/nvchad/lua/mappings.lua
+++ nvim-conf/nvchad/lua/mappings.lua
@@ -10,16 +10,54 @@ end, { desc = "Format code" })
 map("i", "jj", "<esc>")
 map("i", "jk", "<esc>")
 
+-- Manage linter
+nomap("n", "]d")
+nomap("n", "[d")
+map("n", "]l", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+map("n", "[l", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+
 -- want to execute lines
 map("n", "<leader>xx", '<cmd>exec "!".getline(".")<cr>', { desc = "Exec current line" })
 map("n", "<leader>xc", '<cmd>exec ".!".getline(".")<cr>', { desc = "Exec and replace current line" })
 
--- want to parse json
-map("n", "<leader>fj", "<cmd>.!python3 -m json.tool<cr>", { desc = "Format JSON" })
+-- want to format files
+map("n", "<leader>j", "<cmd>.!python3 -m json.tool<cr>", { desc = "Format JSON" })
+map("n", "<leader>p", function()
+  require("conform").format { lsp_fallback = true }
+end, { desc = "format files" })
 
 -- want to clear the search
 nomap("n", "<leader>/")
 map("n", "<leader>/", "<silent><cmd>let @/=''<cr>")
+
+-- want to comment the right way
+nomap("n", "<leader>cc")
+map("n", "<leader>cc", "<Plug>(comment_toggle_linewise_count)")
+map("n", "<leader>cs", "<Plug>(comment_toggle_blockwise_count)")
+map("v", "<leader>cs", "<Plug>(comment_toggle_blockwise_visual)")
+
+-- want to use leader f for ranger-like and move telescope elsewhere
+map("n", "<leader>f", "<cmd>lua require('lf').start()<CR>", { desc = "open lf as file explorer" })
+nomap("n", "<leader>fm")
+nomap("n", "<leader>fa")
+nomap("n", "<leader>fw")
+nomap("n", "<leader>fb")
+nomap("n", "<leader>ff")
+nomap("n", "<leader>fh")
+nomap("n", "<leader>fo")
+nomap("n", "<leader>fz")
+nomap("n", "<leader>pt")
+map(
+  "n",
+  "<leader>ga",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "telescope find all files" }
+)
+map("n", "<leader>gb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>gf", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+map("n", "<leader>gh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+map("n", "<leader>gz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+map("n", "<leader>gw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
 
 local nvim_tmux_nav = require "nvim-tmux-navigation"
 map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
--- nvim-conf/nvchad/lua/plugins/init.lua
+++ nvim-conf/nvchad/lua/plugins/init.lua
@@ -44,6 +44,49 @@ local plugins = {
     },
   },
   { "alexghergh/nvim-tmux-navigation" },
+  { "akinsho/toggleterm.nvim" },
+  {
+    "lmburns/lf.nvim",
+    lazy = false,
+    config = function()
+      -- This feature will not work if the plugin is lazy-loaded
+      vim.g.lf_netrw = 1
+
+      require("lf").setup {
+        escape_quit = false,
+        border = "rounded",
+      }
+
+      vim.keymap.set("n", "<C-f>", "<Cmd>Lf<CR>")
+    end,
+    requires = { "toggleterm.nvim" },
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {
+      mappings = false,
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+      },
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    enabled = false,
+  },
 }
 
 return plugins
