5f8deb0df142b6cb1c957f4286fc52ba5983d669
Pierre Maoui <pierre@qwarry.com>
2025-03-13 14:28:48 +0100

    feat(nvim): avante is capable of creating the bedrock token

--- nvim-conf/nvchad/lua/plugins/init.lua
+++ nvim-conf/nvchad/lua/plugins/init.lua
@@ -34,6 +34,7 @@ local plugins = {
       ensure_installed = {
         "lua",
         "html",
+        "markdown",
         "javascript",
         "typescript",
         "go",
@@ -49,7 +50,7 @@ local plugins = {
   {
     "lmburns/lf.nvim",
     lazy = false,
-    config = function()
+    init = function()
       -- This feature will not work if the plugin is lazy-loaded
       vim.g.lf_netrw = 1
       require("nvchad.term")
@@ -105,6 +106,35 @@ local plugins = {
   },
   {
     "yetone/avante.nvim",
+    config = function()
+      local command = [[
+        aws sts assume-role \
+          --role-arn arn:aws:iam::917060388293:role/PierreNeoVimAvanteBedrockRole \
+          --role-session-name MySession \
+          --region eu-west-3 \
+          --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+          --output text \
+          | awk '{print $1","$2",eu-west-3,"$3}'
+      ]]
+
+      local handle = io.popen(command)
+      if handle == nil then
+        vim.notify("Failed to run AWS command", vim.log.levels.ERROR)
+        return
+      end
+
+      local result = handle:read("*a")
+      handle:close()
+
+      result = result:gsub("%s+", "")
+      if result == nil or result == "" then
+        vim.notify("Failed to retrieve BEDROCK_KEYS", vim.log.levels.ERROR)
+        return
+      end
+
+      vim.env.BEDROCK_KEYS = result
+      vim.notify("BEDROCK_KEYS environment variable set", vim.log.levels.INFO)
+    end,
     event = "VeryLazy",
     detachedHead = false,
     lazy = true,
